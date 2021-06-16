5a0d9e96df01a6e8eb8b43f812b7999d8582ea3e
NickDomanti <domanti.nic@gmail.com>
2021-06-16 18:54:04 +0200

    Add project files.

---
+++ Giorgio/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Giorgio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.label1.Location = new System.Drawing.Point(118, 514);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(694, 221);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "giorgio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Giorgio.Properties.Resources.GiorgioImage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(960, 779);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Giorgio";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Giorgio/Form1.cs
@@ -0,0 +1,84 @@
+﻿using Giorgio.Properties;
+using System;
+using System.Drawing;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giorgio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+
+            new SoundPlayer(Resources.GiorgioAudio).Play();
+
+            for (int i = 0; i < 5; i++)
+                await Task.Delay(1100); // Aspetta il drop della canzone
+
+            await PerformMovementAsync(50, () =>
+            {
+                Left -= 2;
+                Top++;
+            });
+
+            while (true)
+            {
+                await PerformMovementAsync(50, () =>
+                {
+                    Left -= 2;
+                    Top--;
+                });
+                await PerformMovementAsync(50, () =>
+                {
+                    Left += 2;
+                    Top--;
+                });
+
+                await PerformMovementAsync(100, () =>
+                {
+                    Left += 2;
+                    Top++;
+                });
+
+                await PerformMovementAsync(50, () =>
+                {
+                    Left += 2;
+                    Top--;
+                });
+                await PerformMovementAsync(50, () => 
+                { 
+                    Left -= 2; 
+                    Top--; 
+                });
+
+                await PerformMovementAsync(100, () => 
+                {
+                    Left -= 2;
+                    Top++;
+                });
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private async Task PerformMovementAsync(int count, Action action, int delay = 10)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action.Invoke();
+                await Task.Delay(delay);
+            }
+        }
+    }
+}
